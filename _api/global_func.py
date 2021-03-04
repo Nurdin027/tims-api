25227d26152d8e503e83f2225142ad7545484d4b
backend <amarnurdin000@gmail.com>
2021-03-04 11:58:34 +0700

    add global function

--- _api/global_func.py
+++ _api/global_func.py
@@ -1,5 +1,12 @@
+import base64
+import imghdr
+import os
+import random
+import string
+from pathlib import Path
 from pydoc import locate
 
+from flask import current_app
 from flask_restful import reqparse
 
 
@@ -43,3 +50,23 @@ def hapus_field(field):
         print("Error: {}".format(e))
         db.session.rollback()
         return {"message": "Fail"}, 400
+
+
+def rd_str(size=11, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def add_profile_pic(pic_upload, account_id):
+    filename = base64.b64decode(pic_upload)
+    extension = imghdr.what(None, h=filename)
+    storage_filename = rd_str() + '.' + extension
+
+    p = os.path.join(current_app.root_path, 'static/upload/profile/' + account_id)
+    if not os.path.exists(p):
+        os.makedirs(p)
+
+    dir_name = Path(str(p) + '/' + storage_filename)
+    with open(dir_name, 'wb') as f:
+        f.write(filename)
+
+    return storage_filename
